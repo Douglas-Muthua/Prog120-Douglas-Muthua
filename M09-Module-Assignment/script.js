0036e8570e10d12e61b053a21f936dc118cc9598
Douglas-Muthua <103485345+Douglas-Muthua@users.noreply.github.com>
2023-06-03 22:51:36 -0700

    Add files via upload

---
+++ M09-Module-Assignment/script.js
@@ -0,0 +1,53 @@
+var mybutton = document.querySelector("button");
+mybutton.addEventListener("click", function(event) {
+
+
+  var element = document.getElementsByTagName("div");
+  for (index = element.length - 1; index >= 0; index--) {
+    element[index].parentNode.removeChild(element[index]);
+  }
+
+  // Let us stop the propagation of events
+
+  event.stopPropagation();
+});
+addEventListener("click", function(event) {
+  var dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = (event.pageX - 4) + "px";
+  dot.style.top = (event.pageY - 4) + "px";
+  document.body.appendChild(dot);
+  
+
+
+  
+});
+
+
+
+
+// var mybutton2 = document.querySelector("button :nth-child(2)");
+// mybutton.addEventListener("click", function(event) {
+
+
+//   var element = document.getElementById("red");
+//   for (index = element.length - 1; index >= 0; index--) {
+//     element[index].parentNode.removeChild(element[index]);
+//   }
+
+//   // Let us stop the propagation of events
+
+//   event.stopPropagation();
+// });
+
+// addEventListener("click", function(event) {
+//   var dot = document.createElement("div");
+//   dot.className = "red";
+//   dot.style.left = (event.pageX - 4) + "px";
+//   dot.style.top = (event.pageY - 4) + "px";
+//   document.body.appendChild(dot);
+
+ 
+
+  
+// });
